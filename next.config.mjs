5b4c48b9cf66697dd0d0aa11e5e70baccec9c0ba
Santosh Ganji <santoshganji99@gmail.com>
2025-02-21 12:21:31 +0530

    config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value:
+              "frame-ancestors 'self' https://pagesense.zoho.in https://pagesense-proxy.in;",
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
